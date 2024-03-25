4e07d0128fd7b9a71d419a635a4e5168c3d0b414
Vladislav <mvd-96@mail.ru>
2024-03-25 13:56:34 +0300

    HW9

--- Program.cs
+++ Program.cs
@@ -6,9 +6,7 @@ using System.Xml.Serialization;
 using System.Text.Json;
 
 namespace Lesson_009Serialisation
-{
-    internal class Program
-    {/*
+{/*
      * С сайта о погоде была получена информация о текущей и прошлой (за три дня) погоде в виде JSON. 
      * Напишите класс способный хранить представленную информацию.
      * {"Current":{"Time":"2023-06-18T20:35:06.722127+04:00","Temperature":29,"Weathercode":1,"Windspeed":2.1,"Winddirection":1},"History":[{"Time":"2023-06-17T20:35:06.77707+04:00","Temperature":29,"Weathercode":2,"Windspeed":2.4,"Winddirection":1},{"Time":"2023-06-16T20:35:06.777081+04:00","Temperature":22,"Weathercode":2,"Windspeed":2.4,"Winddirection":1},{"Time":"2023-06-15T20:35:06.777082+04:00","Temperature":21,"Weathercode":4,"Windspeed":2.2,"Winddirection":1}]}
